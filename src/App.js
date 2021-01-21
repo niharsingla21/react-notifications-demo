5f0465f1db20c1d3a9b38d46942ff9b8ae14f5c4
Nihar Singla <nihar.singla@mettl.com>
2021-01-21 16:09:44 +0530

    react notifications project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { toast } from 'react-toastify';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>Something went wrong!!
+    <button onClick={closeToast}>Close</button>
+    </div>
+  )
+}
+
+toast.configure()
 function App() {
+
+  const notify = () => {
+    toast('Basic notification!', {position: toast.POSITION.TOP_LEFT, autoClose:8000})
+    toast.success('Success notification!', {position: toast.POSITION.BOTTOM_LEFT, autoClose: false})
+    toast.info('Info notification !', {position: toast.POSITION.TOP_RIGHT})
+    toast.warn(<CustomToast/>, {position: toast.POSITION.BOTTOM_RIGHT})
+    toast.error('Error notification!', {position: toast.POSITION.TOP_CENTER})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={notify}>Notify!</button>
     </div>
   );
 }
